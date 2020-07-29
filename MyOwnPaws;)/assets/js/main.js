519b84410d58f695f20d6740b220d9d10fb4ff9a
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-07-29 22:01:15 +0300

    completed

--- MyOwnPaws;)/assets/js/main.js
+++ MyOwnPaws;)/assets/js/main.js
@@ -16,20 +16,20 @@ function mousemove(event) {
     if (document.attachEvent != null) {
         mouse_x = window.event.clientX;
         mouse_y = window.event.clientY;
+
         check(+mouse_x, +mouse_y, +mouseX, +mouseY);
+
         mouseX = mouse_x;
         mouseY = mouse_y;
-
-
 
     } else if (!document.attachEvent && document.addEventListener) {
         mouse_x = event.clientX;
         mouse_y = event.clientY;
+
         check(+mouse_x, +mouse_y, +mouseX, +mouseY);
+
         mouseX = mouse_x;
         mouseY = mouse_y;
-
-
     }
 
     box.style.top = mouse_y + "px";
@@ -38,16 +38,20 @@ function mousemove(event) {
 
 let pawsDirection = document.querySelectorAll(".paws");
 
-function check(m_x, m_y, mX, MY) {
+
+function check(m_x, m_y, mX, mY) {
     // console.log(f);
     if (mX < m_x) {
         console.log("right");
-        pawsDirection.forEach(paw => paw.style.transform = "rotate(35deg)");
-    } else {
-        console.log("next");
+        pawsDirection.forEach(paw => paw.style.transform = "rotate(60deg)");
+        pawsDirection[0].style.left = "-80px";
+        pawsDirection[1].style.left = "-50px";
+    } else if (mX > m_x) {
+        console.log("left");
+        pawsDirection.forEach(paw => paw.style.transform = "rotate(-60deg)");
+        pawsDirection[0].style.left = "30px";
+        pawsDirection[1].style.left = "0px";
     }
 }
-
-
 
 init();
